95bd07d242f0ce097ff75975f41b7f1a6ac50b17
Mhayk Whandson <hi@mhayk.com>
2021-06-22 16:02:13 +0100

    The context has been imported in the home and room page.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+
+import { auth, firebase } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -7,16 +10,22 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
+import { AuthContext } from '../App'
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-    function navigateToNewRoom() {
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
         history.push('rooms/new')
     }
 
     return (
         <div id="page-auth">
+
             <aside>
                 <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
                 <strong>Create class of Q&amp;A on live</strong>
@@ -25,7 +34,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Google's logo" />
                         Create your room with Google
                     </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -7,8 +7,12 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
+import { useContext } from 'react'
+import { AuthContext } from '../App'
 
 export function NewRoom() {
+    const { user } = useContext(AuthContext)
+
     return (
         <div id="page-auth">
             <aside>
@@ -19,6 +23,7 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
                     <h2>Create a new class</h2>
 
                     <form action="">
