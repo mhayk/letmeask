dc27dc6fd809d99a1dff2d3cdb6d316da918171e
Mhayk Whandson <hi@mhayk.com>
2021-06-22 11:23:32 +0100

    The home page has been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,30 +2,34 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
                 <strong>Create class of Q&amp;A on live</strong>
                 <p>Answer your audience's questions in real-time</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Google's logo" />
                         Create your room with Google
                     </button>
-                    <div>or join in a classroom</div>
+                    <div className="separator">or join in a class</div>
                     <form action="">
                         <input
                             type="text"
                             placeholder="Enter with the class code"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Join class
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
