d233c70587a29ddf5c285de7ce52343641804f81
Mhayk Whandson <hi@mhayk.com>
2021-06-23 19:22:58 +0100

    The flow to allow us enter in a class has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,17 +9,39 @@ import { Button } from '../components/Button'
 import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
+import { FormEvent } from 'react'
+import { useState } from 'react'
+import { database } from '../services/firebase'
 
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() {
         if (!user) {
             await signInWithGoogle()
         }
         history.push('rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.')
+            return
+        }
+
+        history.push(`/rooms/${roomCode}`)
+
     }
 
     return (
@@ -38,10 +60,12 @@ export function Home() {
                         Create your room with Google
                     </button>
                     <div className="separator">or join in a class</div>
-                    <form action="">
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Enter with the class code"
+                            onChange={(e) => setRoomCode(e.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Join class
